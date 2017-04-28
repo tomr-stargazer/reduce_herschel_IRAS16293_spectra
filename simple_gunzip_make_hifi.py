9dc581adfdedcade61d4f18b8c3016b2649ba1ed
Tom Rice <t.rice90@gmail.com>
2017-04-28 16:26:51 -0400

    turned this proof of concept into a real module

--- simple_gunzip_make_hifi.py
+++ simple_gunzip_make_hifi.py
@@ -10,6 +10,7 @@ This is my proof-of-concept that I could, in principle, do the following even ON
 
 from __future__ import division
 import os
+import glob
 from subprocess import call
 
 convert_fits_scriptname = "convert_fits_to_hifi.class"
@@ -30,34 +31,45 @@ def write_class_script_to_convert_FITS_to_HIFI(location, fits_filename, hifi_fil
     return path_to_script_with_filename
 
 
-destination_folder = "/Users/tsrice/Documents/Data/Herschel_Science_Archive/IRAS16293/level_2_5_all_bands/6b/level2_5/myDecon/myDecon_WBS-H"
-fits_gz_filename = "hhifiwbshssb1342191794_25ssv20_1461136819393.fits.gz"
-fits_filename = fits_gz_filename.rstrip(".gz")
-fits_gz_fullpath = os.path.join(destination_folder, fits_gz_filename)
+def convert_FITS_to_HIFI(destination_folder, output_filename):
 
-output_filename = "6b-horizontal.hifi"
-output_fullpath = os.path.join(destination_folder, output_filename)
+    original_directory = os.getcwd()
+
+    try: 
+        os.chdir(destination_folder)
+
+        # glob.glob() is like calling "ls" at the command line
+        # assume there's only gonna be one .fits file here if any
+        if len(glob.glob("*.fits")) == 0:
+            # grab the shortest filename among .fits.gz (this helps avoid any backup files)
+            fits_gz_filename = sorted(glob.glob("*.fits.gz"), key=len)[0]
+            call(["gunzip", fits_gz_filename])
+        fits_filename = glob.glob("*.fits")[0]
+
+        # then make a script and execute it to generate the .hifi file
+        output_fullpath = os.path.join(destination_folder, output_filename)
+        if not os.path.exists(output_fullpath):
+            convert_fits_script_filename = write_class_script_to_convert_FITS_to_HIFI(destination_folder, fits_filename, output_filename)
+            call(["class", "@"+convert_fits_script_filename])
+
+    except Exception as e:
+        os.chdir(original_directory)
+        raise e
+    finally:
+        os.chdir(original_directory)
+
+    return output_fullpath
 
 
-original_directory = os.getcwd()
+if __name__ == "__main__":
 
-try: 
-    os.chdir(destination_folder)
+    destination_folder = "/Users/tsrice/Documents/Data/Herschel_Science_Archive/IRAS16293/level_2_5_all_bands/7a/level2_5/myDecon/myDecon_WBS-V"
+    # fits_gz_filename = "hhifiwbshssb1342191794_25ssv20_1461136819393.fits.gz"
+    # fits_filename = fits_gz_filename.rstrip(".gz")
+    # fits_gz_fullpath = os.path.join(destination_folder, fits_gz_filename)
 
-    # first let's gunzip the file, if needed
-    if not os.path.exists(fits_gz_fullpath):
-        call(["gunzip", fits_gz_filename])
+    output_filename = "7a-vertical.hifi"
+    # output_fullpath = os.path.join(destination_folder, output_filename)
 
-    # then make a script and execute it to generate the  
-    if not os.path.exists(output_fullpath):
-        convert_fits_scriptname = write_class_script_to_convert_FITS_to_HIFI(destination_folder, fits_filename, output_filename)
-        call(["class", "@"+convert_fits_scriptname])
-
-except Exception as e:
-    os.chdir(original_directory)
-    raise e
-finally:
-    os.chdir(original_directory)
-
-
+    convert_FITS_to_HIFI(destination_folder, output_filename)
 
