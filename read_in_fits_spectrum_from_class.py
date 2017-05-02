339ea77083f410bb5e90ba665de607138acb587c
Tom Rice <t.rice90@gmail.com>
2017-05-02 15:27:20 -0400

    convert tabs to spaces

--- read_in_fits_spectrum_from_class.py
+++ read_in_fits_spectrum_from_class.py
@@ -11,25 +11,25 @@ import astropy.io.fits
 
 def load_a_spectrum(filename):
 
-	data, header = getdata(filename, header=True)
+    data, header = getdata(filename, header=True)
 
-	if type(data) == astropy.io.fits.fitsrec.FITS_rec:
-		raise ValueError("This only accepts FITS spectra written in `/mode SPECTRUM`, not `/mode INDEX`.")
+    if type(data) == astropy.io.fits.fitsrec.FITS_rec:
+        raise ValueError("This only accepts FITS spectra written in `/mode SPECTRUM`, not `/mode INDEX`.")
 
-	central_freq = header['RESTFREQ']
-	reference_pixel = header['CRPIX1']
-	delta_freq = header['CDELT1']
-	delta_v = header['DELTAV']
+    central_freq = header['RESTFREQ']
+    reference_pixel = header['CRPIX1']
+    delta_freq = header['CDELT1']
+    delta_v = header['DELTAV']
 
-	spectrum = data[0][0][0]
-	freq_array = (
-		(np.arange(0, len(spectrum)) * delta_freq) + 
-		(central_freq - (reference_pixel*delta_freq)))
-	# downshift to km/s.
-	vel_array = (0.001 * (
-		(np.arange(0, len(spectrum)) * delta_v) + 
-		(0 - reference_pixel*delta_v) ) )
+    spectrum = data[0][0][0]
+    freq_array = (
+        (np.arange(0, len(spectrum)) * delta_freq) + 
+        (central_freq - (reference_pixel*delta_freq)))
+    # downshift to km/s.
+    vel_array = (0.001 * (
+        (np.arange(0, len(spectrum)) * delta_v) + 
+        (0 - reference_pixel*delta_v) ) )
 
-	# for now we'll return these things, but eventually we'll maybe return an Object?
-	# or at least the raw data/header thingies too?
-	return spectrum, freq_array, vel_array
+    # for now we'll return these things, but eventually we'll maybe return an Object?
+    # or at least the raw data/header thingies too?
+    return spectrum, freq_array, vel_array
