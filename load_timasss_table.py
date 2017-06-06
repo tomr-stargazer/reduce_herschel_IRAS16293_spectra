f40bcfa5d3231ed0d6623b1f12fbe2a16faaee34
Tom Rice <t.rice90@gmail.com>
2017-06-06 16:02:03 -0400

    corrected the weird pathname

--- load_timasss_table.py
+++ load_timasss_table.py
@@ -1,5 +1,5 @@
 """
-Literally just loads the table so that I get the correct outcome.
+Literally just loads the table so that I get the correct output.
 
 Three S's. 
 The Iras16293-2422 Millimeter And Submillimeter Spectral Survey.
@@ -9,7 +9,7 @@ The Iras16293-2422 Millimeter And Submillimeter Spectral Survey.
 import os
 import astropy.table
 
-path_to_timasss_table = os.path.expanduser("~/Documents/Code/reduce_herschel_IRAS16293_spectra/timass_table")
+path_to_timasss_table = os.path.expanduser("~/Documents/Code/reduce_herschel_IRAS16293_spectra/timasss_table")
 
 timasss_table = astropy.table.Table.read(os.path.join(path_to_timasss_table, "just_the_table.html"), data_start=2, header_start=0)
 
