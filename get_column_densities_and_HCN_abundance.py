072a15c22852763cdb45c42fb2303a1332c2ed27
Tom Rice <t.rice90@gmail.com>
2017-06-08 15:43:08 -0400

    made the get-c18o fn

--- get_column_densities_and_HCN_abundance.py
+++ get_column_densities_and_HCN_abundance.py
@@ -7,10 +7,10 @@ import numpy as np
 import astropy.units as u
 
 from fit_the_lines_script import baseline_spectra_and_compute_fits
-from linefit_table import make_linefit_table
+from linefit_table import make_linefit_table, make_co_linefit_table
 
 from load_timasss_table import timasss_table
-from groundbased_linefits import make_derived_props_table_ground, select_molecules, enhance_groundtable_with_mol_Ju_cols
+from groundbased_linefits import make_derived_props_table_ground, select_molecules, enhance_groundtable_with_mol_Ju_cols, make_co_derived_props_table_ground
 from make_derived_line_properties_table import make_derived_props_table
 
 def get_N_HCN():
@@ -41,5 +41,37 @@ def get_N_HCN():
 
     return N_HCN
 
+from c18o_fit_the_lines_script import co_baseline_spectra_and_compute_fits
+from c18o_derived_line_properties_table import co_make_derived_props_table
+
+# ok now for the other shoe, metaphorically
+def get_N_C18O():
+
+    herschel_fit_tuple = co_baseline_spectra_and_compute_fits(verbose=False)
+    herschel_linefit_table = make_co_linefit_table(herschel_fit_tuple)
+    herschel_lineprops = co_make_derived_props_table(herschel_linefit_table)
+
+    ground_co_table = select_molecules(timasss_table, ['C18O', 'C17O'])
+    enhance_groundtable_with_mol_Ju_cols(ground_co_table)
+    ground_co_higher_lines= ground_co_table[ground_co_table['Ju']>1]
+    ground_lineprops = make_co_derived_props_table_ground(ground_co_higher_lines)
+
+    f_c = 1.6
+    # carbon_12c_13c_ratio = 70
+    N_C18O = f_c * (
+                np.sum(herschel_lineprops['N(c18o)_upper']) + 
+                np.sum(ground_lineprops['N(c18o)_upper']) )*u.cm**-2
+
+    print("N_C18O = {:.3e}".format(N_C18O))
+
+    return N_C18O
 
 
+def get_N_H2(N_C18O):
+
+    canonical_ratio = 500e4 # from Plume 2012
+    N_H2 = canonical_ratio * N_C18O
+
+    print("N_H2 = {:.3e}".format(N_H2))
+
+    return N_H2
